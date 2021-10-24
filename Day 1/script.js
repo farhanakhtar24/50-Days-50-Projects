a5ca2d66b88687a012cfa9d523df861103d1e7cd
Mohd Farhan Akhtar <akhtar.farhan779@gmail.com>
2021-10-24 18:12:45 +0530

    d9one

--- Day 1/script.js
+++ Day 1/script.js
@@ -1,15 +1,31 @@
+
+// BRUTE FORCE APPROACH
+// const removeActiveClasses = function () {
+//     panels.forEach((panel) => {
+//         panel.classList.remove("active");
+//     })
+// }
+
+// panels.forEach((panel) => {
+//     panel.addEventListener("click", function () {
+//         removeActiveClasses();
+//         panel.classList.add("active");
+//     })
+// })
+
+// Using Event delegation 
+
+const main_container = document.querySelector(".container-main");
 const panels = document.querySelectorAll(".panel");
 
-const removeActiveClasses = function () {
-    panels.forEach((panel) => {
-        panel.classList.remove("active");
-    })
-}
 
-panels.forEach((panel) => {
-    panel.addEventListener("click", function () {
-        removeActiveClasses();
-        panel.classList.add("active");
-    })
+main_container.addEventListener("click", function (panel) {
+
+    // Matching Strategy 
+    const clicked = panel.target.closest(".panel");
+    if (clicked) {
+        panels.forEach((eachPanel) => eachPanel.classList.remove("active"));
+        clicked.classList.add("active");
+    }
 })
 
